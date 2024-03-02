6ca882b906509e38ba49149e7263f43dbb266b8b
Eranda Palliyaguru <74122774+eranda-palliyaguru@users.noreply.github.com>
2024-03-02 12:25:01 +0530

    update

--- api/login.php
+++ api/login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include('../connect.php');
+include('../../connect.php');
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET");
@@ -10,21 +10,35 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 $user=$_POST['user'];
 $password=$_POST['password'];
 
+
+
+$action="not";
+$name='';
+$user_id=0;
+
 $result = $db->prepare("SELECT * FROM user WHERE username='$user' AND password='$password' ");
 $result->bindParam(':userid', $res);
 $result->execute();
-for($i=0; $row = $result->fetch(); $i++){
-    $result_array[] = array (
-        "position" => $row['position'],
-        'name' => $row['name'],
-        "action"=>'ok',
-        "user_id"=>$row['EmployeeId'],
-        "key" => "0000"
-    );
-    }
+for($i=0; $row = $result->fetch(); $i++){ 
+    $user_id=$row['EmployeeId'];
+    $name=$row['name'];
+    $action="ok";
+ }
  
 
+ $key=sha1(date('syihadm'));
 
+
+    
+
+
+
+$result_array = array ("action" => $action,
+                       "user_id" => $user_id,
+                       "name" => $name,
+                       "key" => $key);
 
 
 echo (json_encode ( $result_array ));
+
+?>
