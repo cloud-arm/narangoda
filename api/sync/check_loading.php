e7014041116e3a5380c6a8930df791e41fc6a880
Eranda Palliyaguru <74122774+eranda-palliyaguru@users.noreply.github.com>
2024-03-02 14:14:24 +0530

    update loading check

---
+++ api/sync/check_loading.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+include('../../connect.php');
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+//echo $r_data[0];
+$id=$_POST['id'];
+
+$load_id=1;
+
+$result = $db->prepare("SELECT * FROM loading WHERE driver='$id' AND action='load' ");
+$result->bindParam(':userid', $res);
+$result->execute();
+for($i=0; $row = $result->fetch(); $i++){
+    $result_array[] = array (
+        "action" => $row['action'],
+        "sync" => $row['sync']
+    );
+    }
+ 
+
+
+
+
+echo (json_encode ( $result_array ));
