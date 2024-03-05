b509388224e70c38efc9ed5964bc8f16f1dfbff1
ashen <hansakagaa@gmail.com>
2024-03-05 21:11:41 +0530

    up set sales

--- api/sync/set_sales.php
+++ api/sync/set_sales.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 include('../../connect.php');
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
@@ -7,47 +6,90 @@ header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 
-$user_id = $_SESSION['SESS_MEMBER_ID'];
-$user_name = $_SESSION['SESS_FIRST_NAME'];
+$time = date('H:i');
 
-$invo = $user_id . date("ymdhis");
+// get values
+$load = $_POST['load_id'];
+$cus = $_POST['customer'];
+$invoice = $_POST['invoice'];
+$amount = $_POST['amount'];
+$vat_action = $_POST['vat_action'];
+$vat_no = $_POST['vat_no'];
+$date = $_POST['date'];
 
-$date = date('Y-m-d');
-
-$loading_id = $_POST['id'];
-$cus_id = $_POST['customer'];
-
-
-$result = $db->prepare("SELECT * FROM loading WHERE transaction_id='$loading_id' AND action='load' ");
-$result->bindParam(':userid', $res);
+// get loading details
+$result = $db->prepare("SELECT * FROM loading WHERE transaction_id=:id AND action='load' ");
+$result->bindParam(':id', $load);
 $result->execute();
 for ($i = 0; $row = $result->fetch(); $i++) {
-    $lorry = $row['lorry_no'];;
+    $lorry = $row['lorry_no'];
     $root = $row['root'];;
-    $u_name = $row['driver'];
+    $driver = $row['driver'];
     $term = $row['term'];
 }
 
-
-
-$result = $db->prepare("SELECT * FROM customer WHERE customer_id='$cus_id' ORDER by customer_id DESC ");
-$result->bindParam(':userid', $res);
+// get employee details
+$result = $db->prepare("SELECT * FROM employee WHERE id=:id  ");
+$result->bindParam(':id', $driver);
 $result->execute();
 for ($i = 0; $row = $result->fetch(); $i++) {
-    $cus = $row['customer_name'];
+    $driver_name = $row['name'];
 }
 
+// get customer details
+$result = $db->prepare("SELECT * FROM customer WHERE customer_id=:id  ");
+$result->bindParam(':id', $cus);
+$result->execute();
+for ($i = 0; $row = $result->fetch(); $i++) {
+    $cus_name = $row['customer_name'];
+    $address = $row['address'];
+}
+
+// get sales details
+$result = $db->prepare("SELECT sum(cost_amount),sum(profit),sum(vat),sum(value) FROM sales_list WHERE invoice_no=:id  ");
+$result->bindParam(':id', $invoice);
+$result->execute();
+for ($i = 0; $row = $result->fetch(); $i++) {
+    $cost = $row['sum(cost_amount)'];
+    $profit = $row['sum(profit)'];
+    $vat = $row['sum(vat)'];
+    $value = $row['sum(value)'];
+}
 
 //------------------------------------------------------------------//
-
-
 try {
 
-    $sql = "INSERT INTO sales (invoice_number,cashier,date,name,lorry_no,loading_id,customer_id) VALUES (?,?,?,?,?,?,?)";
+    // insert query
+    $sql = "INSERT INTO sales (invoice_number,cashier,date,time,amount,cost,profit,name,root,rep,lorry_no,term,loading_id,customer_id,action,address,vat,value,cus_vat_no,vat_action) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
     $ql = $db->prepare($sql);
-    $ql->execute(array($invo, $user_name, $date, $cus, $lorry, $loading_id, $cus_id));
+    $ql->execute(array($invoice, $driver, $date, $time, $amount, $cost, $profit, $cus_name, $root, $driver_name, $lorry, $term, $load, $cus, 1, $address, $vat, $value, $vat_no, $vat_action));
 
-    echo "Insert successfully";
+    // get sales  id
+    $result = $db->prepare("SELECT * FROM sales WHERE invoice_number=:id ");
+    $result->bindParam(':id', $invoice);
+    $result->execute();
+    for ($i = 0; $row = $result->fetch(); $i++) {
+        $id = $row['transaction_id'];
+    }
+
+    // create success respond 
+    $result_array[] = array(
+        "cloud_id" => $id,
+        "status" => 'success',
+        "message" => '',
+    );
+
+    // send respond
+    echo (json_encode($result_array));
 } catch (PDOException $e) {
-    echo "Insert failed: " . $e->getMessage();
+
+    // create error respond 
+    $result_array[] = array(
+        "cloud_id" => '0',
+        "status" => 'failed',
+        "message" => $e->getMessage(),
+    );
+
+    // send respond
+    echo (json_encode($result_array));
 }
