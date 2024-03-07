a40e432bf533b92db9f40ffa24053591c5a345fd
ashen <hansakagaa@gmail.com>
2024-03-07 17:34:06 +0530

    init expenses api

---
+++ api/sync/set_expenses.php
@@ -0,0 +1,132 @@
+<?php
+include('../../connect.php');
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
+// get json data
+$json_data = file_get_contents('php://input');
+
+// get values
+$expenses = json_decode($json_data, true);
+
+// respond init
+$result_array = array();
+
+foreach ($expenses as $list) {
+
+    $invoice = $list['invoice_no'];
+    $load_id = $list['loading_id'];
+    $comment = $list['comment'];
+    $amount = $list['amount'];
+    $sub_id = $list['type'];
+    $date = $list['date'];
+    $driver = $list['driver'];
+    $app_id = $list['id'];
+
+    $pay_type = 'cash';
+    $time = date('H:i:s');
+
+    $type = 2;
+    $re = $db->prepare("SELECT * FROM expenses_types WHERE sn=:id ");
+    $re->bindParam(':id', $type);
+    $re->execute();
+    for ($i = 0; $r = $re->fetch(); $i++) {
+        $type_name = $r['type_name'];
+    }
+
+    $re = $db->prepare("SELECT * FROM expenses_sub_type WHERE id=:id ");
+    $re->bindParam(':id', $sub_id);
+    $re->execute();
+    for ($i = 0; $r = $re->fetch(); $i++) {
+        $sub_name = $r['name'];
+    }
+
+    $result = $db->prepare("SELECT * FROM employee WHERE id=:id  ");
+    $result->bindParam(':id', $driver);
+    $result->execute();
+    for ($i = 0; $row = $result->fetch(); $i++) {
+        $driver_name = $row['name'];
+    }
+
+    $acc = 1;
+    $cr_blc = 0;
+    $blc = 0;
+    $re = $db->prepare("SELECT * FROM cash WHERE id = :id");
+    $re->bindParam(':id', $acc);
+    $re->execute();
+    for ($k = 0; $r = $re->fetch(); $k++) {
+        $blc = $r['amount'];
+        $acc_name = $r['name'];
+    }
+
+    $cr_blc = $blc - $amount;
+
+
+    try {
+
+        //checking duplicate
+        $con = 0;
+        $result = $db->prepare("SELECT * FROM expenses_records WHERE invoice_no = '$invoice' AND app_id = '$app_id' AND sub_type = '$sub_id' AND loading_id = '$load_id' ");
+        $result->bindParam(':id', $cus);
+        $result->execute();
+        for ($i = 0; $row = $result->fetch(); $i++) {
+            $con = $row['id'];
+        }
+
+        if ($con == 0) {
+
+            $sql = "UPDATE  cash SET amount=? WHERE id=?";
+            $ql = $db->prepare($sql);
+            $ql->execute(array($cr_blc, $acc));
+
+            $sql = "INSERT INTO transaction_record (transaction_type,type,record_no,amount,action,credit_acc_no,credit_acc_type,credit_acc_name,credit_acc_balance,debit_acc_type,debit_acc_name,debit_acc_id,debit_acc_balance,date,time,user_id,user_name) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            $ql = $db->prepare($sql);
+            $ql->execute(array('expenses', 'Debit', $type, $amount, 0, '', '', '', 0, 'cash_payment', $acc_name, $acc, $cr_blc, $date, $time, $driver, $driver_name));
+
+
+            $sql = "INSERT INTO expenses_records (date,type_id,type,invoice_no,acc_id,acc_name,comment,amount,user,loading_id,pay_type,sub_type,sub_type_name,app_id) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            $q = $db->prepare($sql);
+            $q->execute(array($date, $type, $type_name, $invoice, $acc, $acc_name, $comment, $amount, $driver, $load_id, $pay_type, $sub_id, $sub_name, $app_id));
+        }
+
+
+        // get sales  data
+        $result = $db->prepare("SELECT * FROM expenses_records WHERE invoice_no='$invoice' AND sub_type = '$sub_id' AND loading_id = '$load_id' ");
+        $result->bindParam(':id', $invoice);
+        $result->execute();
+        for ($i = 0; $row = $result->fetch(); $i++) {
+            $id = $row['id'];
+            $ap_id = $row['app_id'];
+            $invo = $row['invoice_no'];
+        }
+
+        // create success respond 
+        $res = array(
+            "cloud_id" => $id,
+            "app_id" => $ap_id,
+            "invoice_no" => $invo,
+            "status" => "success",
+            "message" => "",
+        );
+
+        array_push($result_array, $res);
+    } catch (PDOException $e) {
+
+        // create error respond 
+        $res = array(
+            "cloud_id" => 0,
+            "app_id" => 0,
+            "invoice_no" => "",
+            "status" => "failed",
+            "message" => $e->getMessage(),
+        );
+
+        array_push($result_array, $res);
+    }
+}
+
+// send respond
+echo (json_encode($result_array));
