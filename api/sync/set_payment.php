1577f543d9e11ff7e097ece67872eae2156c3fab
ashen <hansakagaa@gmail.com>
2024-03-07 12:43:51 +0530

    up

--- api/sync/set_payment.php
+++ api/sync/set_payment.php
@@ -33,6 +33,7 @@ foreach ($payment as $list) {
 
     $sales_id = 0;
 
+    $action = 0;
     $credit = 0;
     $pay_amount = 0;
 
@@ -40,15 +41,18 @@ foreach ($payment as $list) {
 
         $credit = $amount;
         $pay_amount = 0;
+        $action = 2;
     } else
 
     if ($pay_type == 'chq') {
 
         $credit = 0;
         $pay_amount = 0;
+        $action = 2;
     } else {
 
         $credit = 0;
+        $action = 1;
         $pay_amount = $amount;
     }
 
@@ -69,7 +73,7 @@ foreach ($payment as $list) {
             // insert query
             $sql = "INSERT INTO payment (invoice_no,pay_amount,amount,type,date,time,chq_no,bank_name,chq_date,chq_action,action,sales_id,customer_id,loading_id,pay_type,chq_bank,credit_balance,app_id) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
             $ql = $db->prepare($sql);
-            $ql->execute(array($invoice, $pay_amount, $amount, $pay_type, $date, $time, $chq_no, '', $chq_date, 0, 0, $sales_id, $cus, $load, $pay_type, $bank, $credit, $app_id));
+            $ql->execute(array($invoice, $pay_amount, $amount, $pay_type, $date, $time, $chq_no, '', $chq_date, 0, $action, $sales_id, $cus, $load, $pay_type, $bank, $credit, $app_id));
         }
 
         // get sales  data
