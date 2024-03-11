393165ae8276a1fccc610f95d72425b9686f66d4
Eranda Palliyaguru <74122774+eranda-palliyaguru@users.noreply.github.com>
2024-03-11 19:07:38 +0530

    q update

--- api/sync/sync_customer.php
+++ api/sync/sync_customer.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include('../connect.php');
+include('../../connect.php');
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET");
