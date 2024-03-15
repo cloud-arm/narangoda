60d7266476fa096c58d1698b7fc78a2e3bd79fd6
ashen <hansakagaa@gmail.com>
2024-03-15 18:45:58 +0530

    up

--- customer_dll.php
+++ customer_dll.php
@@ -2,11 +2,31 @@
 session_start();
 include('connect.php');
 date_default_timezone_set("Asia/Colombo");
+  
+  
+ 
+$id=$_GET['id'];
 
 
-
-$id = $_GET['id'];
 
 $result = $db->prepare("DELETE FROM customer WHERE  customer_id= :memid");
-$result->bindParam(':memid', $id);
-$result->execute();
+	$result->bindParam(':memid', $id);
+	$result->execute();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
