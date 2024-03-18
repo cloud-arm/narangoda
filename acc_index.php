8b6ee19c4fec831da6175e4f8f1c2431e9d9de2d
ashen <hansakagaa@gmail.com>
2024-03-18 07:14:40 +0530

    up

--- acc_index.php
+++ acc_index.php
@@ -50,52 +50,7 @@ include("connect.php");
             date_default_timezone_set("Asia/Colombo");
             $cash = $_SESSION['SESS_FIRST_NAME'];
 
-            $date =  date("Y/m/d");
-
-            $result = $db->prepare("SELECT sum(amount) FROM sales WHERE    date='$date' ");
-
-            $result->bindParam(':userid', $date);
-            $result->execute();
-            for ($i = 0; $row = $result->fetch(); $i++) {
-
-                $amount = $row['sum(amount)'];
-            }
-
-
-
-
-            $result = $db->prepare("SELECT sum(amount) FROM sales WHERE    date='$date' AND cashier='$cash' ");
-
-            $result->bindParam(':userid', $date);
-            $result->execute();
-            for ($i = 0; $row = $result->fetch(); $i++) {
-
-                $amount_cash = $row['sum(amount)'];
-            }
-
-
-
-
-            $result = $db->prepare("SELECT sum(amount) FROM sales WHERE    date='$date' AND cashier='Buddika' ");
-
-            $result->bindParam(':userid', $date);
-            $result->execute();
-            for ($i = 0; $row = $result->fetch(); $i++) {
-
-                $bamount_cash = $row['sum(amount)'];
-            }
-
-            $result = $db->prepare("SELECT * FROM customer  ");
-            $result->bindParam(':userid', $date);
-            $result->execute();
-            $tre = $result->rowcount();
-
-
-            date_default_timezone_set("Asia/Colombo");
             $date = date("Y-m-d");
-            $month = date("Y-M");
-
-
 
             ?>
 
@@ -183,7 +138,7 @@ include("connect.php");
 
 
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-6 hidden">
                     <!-- LINE CHART -->
                     <div class="box box-solid bg-teal-gradient">
                         <div class="box-header">
@@ -204,7 +159,7 @@ include("connect.php");
                     </div>
                 </div>
                 <!-- /.col (LEFT) -->
-                <div class="col-md-6">
+                <div class="col-md-6 hidden">
                     <!-- BAR CHART -->
                     <div class="box box-solid ">
                         <div class="box-header ">
@@ -225,10 +180,6 @@ include("connect.php");
 
                 </div>
                 <!-- /.col (RIGHT) -->
-
-
-
-
 
                 <div class="col-md-12">
                     <!-- BAR CHART -->
@@ -253,231 +204,7 @@ include("connect.php");
 
             </div>
 
-            <div class="row">
-
-
-
-
-                <?php $r = $_SESSION['SESS_LAST_NAME'];
-
-                ?>
-
-
-
-
-
-                <div class="col-md-4">
-                    <!-- Widget: user widget style 1 -->
-                    <div class="box box-widget widget-user-2">
-                        <!-- Add the bg color to the header using any of the bg-* classes -->
-                        <div class="widget-user-header bg-red">
-                            <div class="widget-user-image">
-
-                            </div>
-                            <!-- /.widget-user-image -->
-                            <h3 class="widget-user-username">Stores </h3>
-                            <h5 class="widget-user-desc"></h5>
-                        </div>
-                        <div class="box-footer no-padding">
-                            <ul class="nav nav-stacked">
-                                <?php
-                                $result = $db->prepare("SELECT * FROM products WHERE product_id>=9  ");
-                                $result->bindParam(':userid', $date);
-                                $result->execute();
-                                for ($i = 0; $row = $result->fetch(); $i++) {
-                                ?>
-                                    <li><a href="#"><?php echo $row['gen_name']; ?> <span class="pull-right badge bg-red"><?php echo $row['qty']; ?></span></a></li>
-                                <?php } ?>
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- /.widget-user -->
-                </div>
-
-
-
-
-
-
-
-                <div class="col-md-4">
-                    <!-- Widget: user widget style 1 -->
-                    <div class="box box-widget widget-user-2">
-                        <!-- Add the bg color to the header using any of the bg-* classes -->
-                        <div class="widget-user-header bg-yellow" style="background: url('user_pic/laugh_gas_price_goes_up.jpg') center center;">
-
-
-
-                            <!-- /.widget-user-image -->
-                            <h2 class="widget-user-username">YARD</h2>
-                            <h4 class="widget-user-desc"></h4>
-                        </div>
-                        <div class="box-footer no-padding">
-                            <ul class="nav nav-stacked"><?php
-                                                        $result = $db->prepare("SELECT * FROM products WHERE product_name>=1  ");
-
-                                                        $result->bindParam(':userid', $date);
-                                                        $result->execute();
-                                                        for ($i = 0; $row = $result->fetch(); $i++) {
-
-
-                                                        ?>
-                                    <li><a href="#"><?php echo $row['gen_name']; ?> <span class="pull-right badge bg-red"><?php echo $row['qty']; ?></span></a></li>
-                                <?php } ?>
-                                <?php
-                                $result = $db->prepare("SELECT * FROM products WHERE product_name='' and product_id<9 ");
-                                $result->bindParam(':userid', $date);
-                                $result->execute();
-                                for ($i = 0; $row = $result->fetch(); $i++) {
-                                    $pro_id = $row['product_id'];
-                                    $cha = 0;
-                                    $result1 = $db->prepare("SELECT * FROM products WHERE product_name='$pro_id'  ");
-                                    $result1->bindParam(':userid', $date);
-                                    $result1->execute();
-                                    for ($i = 0; $row1 = $result1->fetch(); $i++) {
-                                        $cha = $row1['qty'];
-                                    }
-
-                                ?>
-                                    <li><a href="#"><?php echo $row['gen_name']; ?><span class="pull-right badge bg-"><?php echo $row['qty'] - $cha; ?></span>
-                                            <span class="pull-right badge bg-green"><?php echo $row['qty']; ?></span>
-                                        </a></li>
-                                <?php } ?>
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- /.widget-user -->
-                </div>
-
-
-
-
-
-
-
-                <div class="col-md-4">
-                    <!-- Widget: user widget style 1 -->
-                    <div class="box box-widget widget-user-2">
-                        <!-- Add the bg color to the header using any of the bg-* classes -->
-                        <div class="widget-user-header bg-purple">
-                            <div class="widget-user-image">
-
-                            </div>
-                            <!-- /.widget-user-image -->
-                            <h3 class="widget-user-username"> Buffer Stock Balance</h3>
-                            <h5 class="widget-user-desc"></h5>
-                        </div>
-                        <div class="box-footer no-padding">
-                            <ul class="nav nav-stacked">
-
-                                <?php
-                                $result = $db->prepare("SELECT * FROM products WHERE  product_id<9 ");
-
-                                $result->bindParam(':userid', $date);
-                                $result->execute();
-                                for ($i = 0; $row = $result->fetch(); $i++) {
-
-
-                                ?>
-                                    <li><a href="#"><?php echo $row['gen_name']; ?>
-                                            <span class="pull-right badge bg-red"><?php echo $row['qty'] - $row['qty2']; ?></span>
-                                            <span class="pull-right badge bg-aqua"><?php echo $row['qty2']; ?></span>
-                                        </a></li>
-
-                                <?php } ?>
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- /.widget-user -->
-                </div>
-            </div>
-
-            <div class="row">
-
-                <!-- /----------------------------------------------------.Lorry-view --------------------------------------------------------------- -->
-
-                <?php
-                $result = $db->prepare("SELECT * FROM lorry WHERE action='load'  ");
-
-                $result->bindParam(':userid', $date);
-                $result->execute();
-                for ($i = 0; $row = $result->fetch(); $i++) {
-                    $no = $row['lorry_no'];
-                    $lo_id = $row['loading_id'];
-
-
-
-                    $result11 = $db->prepare("SELECT * FROM loading WHERE transaction_id='$lo_id' ");
-                    $result11->bindParam(':userid', $date);
-                    $result11->execute();
-                    for ($i = 0; $row11 = $result11->fetch(); $i++) {
-                        $driver_id = $row11['driver'];
-                        $result1 = $db->prepare("SELECT * FROM employee WHERE  id='$driver_id'  ");
-                        $result1->bindParam(':userid', $date);
-                        $result1->execute();
-                        for ($i = 0; $row1 = $result1->fetch(); $i++) {
-                            $driver = $row1['name'];
-                            $pic = $row1['pic'];
-                        }
-                    }
-
-                ?>
-
-                    <div class="col-md-4">
-                        <!-- Widget: user widget style 1 -->
-
-                        <!-- Widget: user widget style 1 -->
-                        <div class="box box-widget widget-user">
-                            <a href="loading_view.php?id=<?php echo $lo_id; ?>" style="color: black">
-                                <!-- Add the bg color to the header using any of the bg-* classes -->
-                                <div class="widget-user-header bg-black" style="background: url('user_pic/images.jfif') center center;">
-                                    <h2 class="widget-user-username" style="color: black"><?php echo $no; ?></h2>
-                                    <h3 class="widget-user-desc" style="color: darkred"><?php echo $driver; ?></h3>
-                                </div>
-                            </a>
-                            <div class="widget-user-image">
-                                <img class="img-circle" src="<?php echo $pic; ?>" alt="User Avatar">
-                            </div>
-
-                            <div class="box-footer no-padding">
-                                <ul class="nav nav-stacked">
-                                    <?php
-
-                                    $result1 = $db->prepare("SELECT * FROM loading_list WHERE loading_id='$lo_id' ");
-                                    $result1->bindParam(':userid', $date);
-                                    $result1->execute();
-                                    for ($i = 0; $row1 = $result1->fetch(); $i++) {
-                                        if ($row1['product_code'] > 4) {
-                                    ?> <li style="background: url('user_pic/download.jfif') center;"> <?php
-                                                                                                    } else {
-                                                                                                        echo ' <li >';
-                                                                                                    }
-
-                                                                                                        ?>
-                                            <a><span class="badge bg-black"><?php echo $row1['product_name']; ?></span> <span class="pull-right badge "><?php echo $row1['qty_sold']; ?></span>
-                                                <span class="pull-right badge bg-red"><?php echo $row1['qty']; ?></span>
-                                            </a>
-                                            </li>
-                                        <?php } ?>
-
-                                </ul>
-                            </div>
-                        </div>
-                        <!-- /.widget-user -->
-                    </div>
-                <?php
-                }
-                ?>
-
-
-                <!-- ./col -->
-            </div>
-
-
-
-
         </section>
-    </div>
     </div>
     <!-- /.content -->
 
@@ -489,6 +216,7 @@ include("connect.php");
     <!-- Add the sidebar's background. This div must be placed
        immediately after the control sidebar -->
     <div class="control-sidebar-bg"></div>
+    </div>
 
     <!-- ./wrapper -->
 
