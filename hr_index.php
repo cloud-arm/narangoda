87a46320e7eaa28188d18fda3b74aea1b1332ec8
ashen <hansakagaa@gmail.com>
2024-03-19 15:49:14 +0530

    up index

--- hr_index.php
+++ hr_index.php
@@ -36,6 +36,10 @@ include("connect.php");
       <h1>
         Home
         <small>Preview</small>
+        <div class="pull-right" style="margin-right: 25px;">
+          <span style="font-size:15px;"><i class="fa fa-circle text-yellow"></i> Driver</span>
+          <span style="font-size:15px;"><i class="fa fa-circle text-green"></i> Helper</span>
+        </div>
       </h1>
 
     </section>
@@ -51,10 +55,9 @@ include("connect.php");
       $cash = $_SESSION['SESS_FIRST_NAME'];
 
       $date = date("Y-m-d");
-      $month = date("Y-M");
       ?>
 
-      <div class="row">
+      <div class="row hidden">
         <div class="col-md-3">
           <!-- Info Boxes Style 2 -->
           <div class="info-box bg-yellow">
@@ -113,99 +116,115 @@ include("connect.php");
             <!-- /.info-box-content -->
           </div>
         </div>
-        <div class="col-md-3">
-          <!-- /.info-box -->
-          <div class="info-box bg-aqua">
-            <span class="info-box-icon"><i class="glyphicon glyphicon-signal"></i></span>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-yellow">
+              <div class="info-box-img">
+                <img src="<?php //echo $driver_pic; 
+                          ?>" alt="">
+              </div>
+            </span>
 
             <div class="info-box-content">
-              <span class="info-box-text">Day Average</span>
-              <span class="info-box-number">163,921</span>
-
-              <div class="progress">
-                <div class="progress-bar" style="width: 20%"></div>
-              </div>
-              <span class="progress-description">
-                40% Increase in 30 Days
-              </span>
+              <span class="info-box-text">Driver</span>
+              <span class="info-box-number" style="margin-top: 10px;"><?php echo '$driver'; ?></span>
             </div>
             <!-- /.info-box-content -->
           </div>
+          <!-- /.info-box -->
         </div>
         <!-- /.info-box -->
 
       </div> <!-- /.box -->
 
+      <div class="row">
+        <?php 
+        $result = $db->prepare("SELECT  * FROM employee WHERE des_id=1  ");
+        $result->bindParam(':userid', $date);
+        $result->execute();
+        for ($i = 0; $row = $result->fetch(); $i++) {
+          $cl1 = '';
+          $cl2 = 'bg-yellow';
+          $id = $row['id'];
+          $res = $db->prepare("SELECT  * FROM attendance WHERE emp_id='$id' AND date = '$date' ");
+          $res->bindParam(':userid', $date);
+          $res->execute();
+          for ($i = 0; $ro = $res->fetch(); $i++) {
+            $cl1 = 'bg-yellow';
+            $cl2 = '';
+          }
+        ?>
+          <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box <?php echo $cl1; ?>">
+              <span class="info-box-icon <?php echo $cl2; ?>">
+                <?php if ($row['pic'] != '') { ?>
+                  <div class="info-box-img">
+                    <img src="<?php echo $row['pic']; ?>" alt="">
+                  </div>
+                <?php } else { ?>
+                  <i class="glyphicon glyphicon-user"></i>
+                <?php } ?>
+              </span>
+
+              <div class="info-box-content">
+                <span class="info-box-text"><?php echo ucfirst($row['username']) ?></span>
+                <span class="info-box-number"><?php echo $row['phone_no']; ?></span>
+
+                <div class="progress">
+                  <div class="progress-bar" style="width: 0"></div>
+                </div>
+                <span class="progress-description">
+                  <?php echo $row['address']; ?>
+                </span>
+              </div>
+            </div>
+          </div>
+        <?php } ?>
+      </div>
 
       <div class="row">
-        <div class="col-md-6">
-          <!-- LINE CHART -->
-          <div class="box box-solid bg-teal-gradient">
-            <div class="box-header">
-              <i class="fa fa-th"></i>
+        <?php 
+        $result = $db->prepare("SELECT  * FROM employee WHERE des_id=2  ");
+        $result->bindParam(':userid', $date);
+        $result->execute();
+        for ($i = 0; $row = $result->fetch(); $i++) {
+          $cl1 = '';
+          $cl2 = 'bg-green';
+          $id = $row['id'];
+          $res = $db->prepare("SELECT  * FROM attendance WHERE emp_id='$id' AND date = '$date' ");
+          $res->bindParam(':userid', $date);
+          $res->execute();
+          for ($i = 0; $ro = $res->fetch(); $i++) {
+            $cl1 = 'bg-green';
+            $cl2 = '';
+          }
+        ?>
+          <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box <?php echo $cl1; ?>">
+              <span class="info-box-icon <?php echo $cl2; ?>">
+                <?php if ($row['pic'] != '') { ?>
+                  <div class="info-box-img">
+                    <img src="<?php echo $row['pic']; ?>" alt="">
+                  </div>
+                <?php } else { ?>
+                  <i class="glyphicon glyphicon-user"></i>
+                <?php } ?>
+              </span>
 
-              <h3 class="box-title">Sales Graph</h3>
+              <div class="info-box-content">
+                <span class="info-box-text"><?php echo ucfirst($row['username']) ?></span>
+                <span class="info-box-number"><?php echo $row['phone_no']; ?></span>
 
-              <div class="box-tools pull-right">
-                <button type="button" class="btn bg-teal btn-sm" data-widget="collapse"><i class="fa fa-minus"></i>
-                </button>
-                <button type="button" class="btn bg-teal btn-sm" data-widget="remove"><i class="fa fa-times"></i>
-                </button>
+                <div class="progress">
+                  <div class="progress-bar" style="width: 0"></div>
+                </div>
+                <span class="progress-description">
+                  <?php echo $row['address']; ?>
+                </span>
               </div>
             </div>
-            <div class="box-body border-radius-none">
-              <div class="chart" id="line-chart" style="height: 300px;"></div>
-            </div>
           </div>
-        </div>
-        <!-- /.col (LEFT) -->
-        <div class="col-md-6">
-          <!-- BAR CHART -->
-          <div class="box box-solid ">
-            <div class="box-header ">
-              <h3 class="box-title">IN AND OUT Chart</h3>
-              <div class="box-tools pull-right">
-                <button type="button" class="btn  btn-sm" data-widget="collapse"><i class="fa fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-sm" data-widget="remove"><i class="fa fa-times"></i>
-                </button>
-              </div>
-            </div>
-            <div class="box-body chart-responsive">
-              <div class="chart" id="bar-chart" style="height: 300px;"></div>
-            </div>
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
-
-        </div>
-        <!-- /.col (RIGHT) -->
-
-
-
-
-
-        <div class="col-md-12">
-          <!-- BAR CHART -->
-          <div class="box box-solid ">
-            <div class="box-header ">
-              <h3 class="box-title">14 Day Payment Chart</h3>
-              <div class="box-tools pull-right">
-                <button type="button" class="btn  btn-sm" data-widget="collapse"><i class="fa fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-sm" data-widget="remove"><i class="fa fa-times"></i>
-                </button>
-              </div>
-            </div>
-            <div class="box-body chart-responsive">
-              <div class="chart" id="bar-chart2"></div>
-            </div>
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
-
-        </div>
-
+        <?php } ?>
       </div>
 
     </section>
@@ -241,172 +260,7 @@ include("connect.php");
   <script src="https://dev.colorbiz.org/ashen/cdn/main/dist/js/DarkTheme.js"></script>
   <!-- page script -->
 
-  <script>
-    $(function() {
-      "use strict";
 
-
-
-      // LINE CHART
-      var line = new Morris.Line({
-        element: 'line-chart',
-        resize: true,
-        data: [{
-            y: '2019 Q1',
-            item1: 26006060
-          },
-          {
-            y: '2019 Q2',
-            item1: 27007080
-          },
-          {
-            y: '2019 Q3',
-            item1: 49001020
-          },
-          {
-            y: '2019 Q4',
-            item1: 37006070
-          },
-          {
-            y: '2020 Q1',
-            item1: 68001000
-          },
-          {
-            y: '2020 Q2',
-            item1: 56007000
-          },
-          {
-            y: '2020 Q3',
-            item1: 48002000
-          },
-          {
-            y: '2020 Q4',
-            item1: 150007030
-          },
-          {
-            y: '2021 Q1',
-            item1: 106008070
-          },
-          {
-            y: '2021 Q2',
-            item1: 84003020
-          }
-        ],
-        xkey: 'y',
-        ykeys: ['item1'],
-        labels: ['Value'],
-        lineColors: ['#ffffff'],
-        gridTextColor: ['#ffffff'],
-        hideHover: 'auto'
-      });
-
-
-
-      //BAR CHART
-      var bar = new Morris.Bar({
-        element: 'bar-chart',
-        resize: true,
-        data: [{
-            y: '2015',
-            a: 10000000,
-            b: 9000000
-          },
-          {
-            y: '2016',
-            a: 7500000,
-            b: 6500000
-          },
-          {
-            y: '2017',
-            a: 5000000,
-            b: 4000000
-          },
-          {
-            y: '2018',
-            a: 7500000,
-            b: 6500000
-          },
-          {
-            y: '2019',
-            a: 5000000,
-            b: 4000000
-          },
-          {
-            y: '2020',
-            a: 7500000,
-            b: 6500000
-          },
-          {
-            y: '2021',
-            a: 7500000,
-            b: 9700000
-          }
-        ],
-        barColors: ['#00a65a', '#f56954'],
-        xkey: 'y',
-        ykeys: ['a', 'b'],
-        labels: ['IN', 'OUT'],
-        hideHover: 'auto'
-      });
-
-
-      //BAR CHART
-      var bar = new Morris.Bar({
-        element: 'bar-chart2',
-        resize: true,
-        data: [
-          <?php $x = 0;
-          while ($x <= 14) {
-            $d = strtotime("-$x Day");
-            $date = date("Y-m-d", $d);
-            $cash = 0;
-            $chq = 0;
-            $credit = 0;
-            $result1 = $db->prepare("SELECT  amount FROM payment WHERE date='$date' AND action > '0' AND type='cash' ");
-            $result1->bindParam(':userid', $date);
-            $result1->execute();
-            for ($i = 0; $row1 = $result1->fetch(); $i++) {
-              $cash += $row1['amount'];
-            }
-            $result1 = $db->prepare("SELECT  amount FROM payment WHERE date='$date' AND action > '0' AND type='chq' ");
-            $result1->bindParam(':userid', $date);
-            $result1->execute();
-            for ($i = 0; $row1 = $result1->fetch(); $i++) {
-              $chq += $row1['amount'];
-            }
-
-            $result1 = $db->prepare("SELECT  amount FROM payment WHERE date='$date' AND action > '0' AND type='credit' ");
-            $result1->bindParam(':userid', $date);
-            $result1->execute();
-            for ($i = 0; $row1 = $result1->fetch(); $i++) {
-              $credit += $row1['amount'];
-            }
-
-            $split = explode("-", $date);
-            $y = $split[0];
-            $m = $split[1];
-            $d = $split[2];
-            $date = mktime(0, 0, 0, $m, $d, $y);
-            $date = date('M d', $date);
-
-          ?> {
-              x: '<?php echo $date; ?>',
-              a: <?php echo $cash; ?>,
-              b: <?php echo $chq; ?>,
-              c: <?php echo $credit; ?>
-            },
-          <?php $x++;
-          } ?>
-
-        ],
-        barColors: ['#ff9900', '#8c8c8c', '#cc0000'],
-        xkey: 'x',
-        ykeys: ['a', 'b', 'c'],
-        labels: ['CASH', 'CHQ', 'CREDIT'],
-        hideHover: 'auto'
-      });
-    });
-  </script>
 </body>
 
 </html>
