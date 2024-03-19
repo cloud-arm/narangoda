2c5d73207bb6f80058f31d03ec3ad523f6384eb9
ashen <hansakagaa@gmail.com>
2024-03-19 06:44:32 +0530

    up

--- customer_category.php
+++ customer_category.php
@@ -1,227 +1,249 @@
 <!DOCTYPE html>
 <html>
-<?php 
+<?php
 include("head.php");
 include("connect.php");
 ?>
+
 <body class="hold-transition skin-blue sidebar-mini">
-<?php 
-include_once("auth.php");
-$r=$_SESSION['SESS_LAST_NAME'];
-if($r =='Cashier'){
-header("location:./../../../index.php");
-}
-if($r =='admin'){
-include_once("sidebar.php");
-}
-?>
+  <?php
+  include_once("auth.php");
+  $r = $_SESSION['SESS_LAST_NAME'];
+  $_SESSION['SESS_FORM'] = 'customer_category';
+  if ($r == 'Cashier') {
+    header("location:./../../../index.php");
+  }
+  if ($r == 'admin') {
+    include_once("sidebar.php");
+  }
+  ?>
 
-
-
-
-<link rel="stylesheet" href="datepicker.css"
-        type="text/css" media="all" />
-    <script src="datepicker.js" type="text/javascript"></script>
-    <script src="datepicker.ui.min.js"
-        type="text/javascript"></script>
- <script type="text/javascript">
-     
-		 $(function(){
-        $("#datepicker1").datepicker({ dateFormat: 'yy/mm/dd' });
-        $("#datepicker2").datepicker({ dateFormat: 'yy/mm/dd' });
-       
-    });
-
-    </script>
-
-
-
-
-    <!-- /.sidebar -->
+  <!-- /.sidebar -->
   </aside>
 
   <!-- Content Wrapper. Contains page content -->
-     <div class="content-wrapper">
+  <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
         CATEGORY
         <small>Preview</small>
       </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Forms</a></li>
-        <li class="active">Advanced Elements</li>
-      </ol>
     </section>
-   
-   
-   
-    
-    
-   
-   
-   
-   <section class="content">
-   
-     <div class="box">
-            <div class="box-header">
-              <h3 class="box-title">Customer Category List</h3>			
-            </div>
-<a rel="facebox" href="customer_category_add.php"><button class="btn btn-info">Add New Category</button></a>
-            <!-- /.box-header -->
-			
-            <div class="box-body">
-            		   <table id="example1" class="table table-bordered table-striped">
-                <thead>
-				
-                <tr>
-				<th >id</th>
-				  <th >Category</th>
-                  
-				   
-				   <th>#</th>
-				  <th>#</th>
-                </tr>
-				</thead>
-				
-                
-                <tbody>
-				<?php
-	    date_default_timezone_set("Asia/Colombo");
-		$hh=date("Y/m/d");
-			
-				//$d3=$_SESSION['SESS_FIRST_NAME'];
-				//$d3=$_GET['d3'];
-				$result2 = $db->prepare("SELECT * FROM customer_category ");
-				
-					$result2->bindParam(':userid', $d2);
-                $result2->execute();
-                for($i=0; $row2 = $result2->fetch(); $i++){
-				
-	
-	
 
-				
-				
-			?>
-                <tr class="record">
-				
-				<td><?php echo $row2['id'];?></td>
-                <td><?php echo $row2['name'];?></td>
-				<td><a href="customer_group.php?id=<?php echo $row2['id']; ?>"  title="Click to ADD"><button class="btn btn-info">
-				 Add Customer </button></a></td>
-				<td>
-					
-					
-					<a href="#" id="<?php echo $row2['id']; ?>" class="delbutton" title="Click to Delete"><button class="btn btn-danger">
-				<i class="fa fa-trash-o"></i></button></a></td>
-				</tr>
-                <?php	}	?>
-                </tbody>
-				
+    <section class="content">
 
-              </table>  
-
-            </div>
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
+      <div class="box">
+        <div class="box-header">
+          <h3 class="box-title">Customer Category List</h3>
+          <small class="btn btn-success btn-sm mx-2" style="padding: 5px 10px;" title="Add New Category" onclick="click_open(1)">Add New Category</small>
         </div>
-        <!-- /.col -->
-      
-   
-   
-   
 
-    <!-- Main content -->
-    
-      <!-- /.row -->
+        <div class="box-body">
+          <table id="example1" class="table table-bordered table-striped">
+            <thead>
+
+              <tr>
+                <th>id</th>
+                <th>Category</th>
+                <th>#</th>
+                <th>#</th>
+              </tr>
+            </thead>
+
+
+            <tbody>
+              <?php
+
+              $result2 = $db->prepare("SELECT * FROM customer_category ");
+              $result2->bindParam(':userid', $d2);
+              $result2->execute();
+              for ($i = 0; $row2 = $result2->fetch(); $i++) { ?>
+                <tr class="record">
+
+                  <td><?php echo $row2['id']; ?></td>
+                  <td><?php echo $row2['name']; ?></td>
+                  <td>
+                    <a href="customer_group.php?id=<?php echo $row2['id']; ?>" title="Click to ADD" class="btn btn-info">
+                      Add Customer GROUP
+                    </a>
+                  </td>
+                  <td>
+                    <a href="#" id="<?php echo $row2['id']; ?>" class="" title="Click to Delete" class="btn btn-danger delbutton">
+                      <i class="fa fa-trash"></i>
+                    </a>
+                  </td>
+                </tr>
+              <?php  }  ?>
+            </tbody>
+
+
+          </table>
+
+        </div>
+        <!-- /.box-body -->
+      </div>
 
     </section>
     <!-- /.content -->
-   </div>
+  </div>
   <!-- /.content-wrapper -->
-<?php
+  <?php
   include("dounbr.php");
-?>
-  <!-- /.control-sidebar -->
-  <!-- Add the sidebar's background. This div must be placed
-       immediately after the control sidebar -->
+  ?>
+
+  <div class="container-up d-none" id="container_up">
+    <div class="container-close" onclick="click_close()"></div>
+    <div class="row">
+      <div class="col-md-12">
+
+        <div class="box box-success popup d-none" id="popup_1" style="width: 500px;">
+          <div class="box-header with-border">
+            <h3 class="box-title w-100">
+              New Customer Category
+              <i onclick="click_close()" class="btn me-2 pull-right fa fa-remove" style="font-size: 25px"></i>
+            </h3>
+          </div>
+
+          <div class="box-body d-block">
+            <form method="POST" action="customer_category_save.php">
+
+              <div class="row" style="display: block;">
+
+                <div class="col-md-9">
+                  <div class="form-group">
+                    <label>Category Name</label>
+                    <input type="text" name="name" class="form-control">
+                  </div>
+                </div>
+
+                <div class="col-md-3">
+                  <div class="form-group" style="display: flex;justify-content:center">
+                    <input type="hidden" name="unit" value="1">
+                    <input type="submit" style="margin-top:23px;" value="Save" class="btn btn-info">
+                  </div>
+                </div>
+              </div>
+
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
   <div class="control-sidebar-bg"></div>
-</div>
-<!-- ./wrapper -->
+  </div>
+  <!-- ./wrapper -->
 
-<!-- jQuery 2.2.3 -->
-<script src="../../plugins/jQuery/jquery-2.2.3.min.js"></script>
-<!-- Bootstrap 3.3.6 -->
-<script src="../../bootstrap/js/bootstrap.min.js"></script>
-<!-- DataTables -->
-<script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="../../plugins/datatables/dataTables.bootstrap.min.js"></script>
-<!-- SlimScroll -->
-<script src="../../plugins/slimScroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="../../plugins/fastclick/fastclick.js"></script>
-<!-- AdminLTE App -->
-<script src="../../dist/js/app.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="../../dist/js/demo.js"></script>
-<script src="../../plugins/datepicker/bootstrap-datepicker.js"></script>
-<!-- page script -->
-<script>
-$(function() {
-$(".delbutton").click(function(){
-//Save the link in a variable called element
-var element = $(this);
-//Find the id of the link that was clicked
-var del_id = element.attr("id");
-//Built a url to send
-var info = 'id=' + del_id;
- if(confirm("Sure you want to delete this Special Price? There is NO undo!"))
-		  {
- $.ajax({
-   type: "GET",
-   url: "special_price_dll.php",
-   data: info,
-   success: function(){
-   
-   }
- });
-         $(this).parents(".record").animate({ backgroundColor: "#fbc7c7" }, "fast")
-		.animate({ opacity: "hide" }, "slow");
 
- }
+  <!-- jQuery 2.2.3 -->
+  <script src="../../plugins/jQuery/jquery-2.2.3.min.js"></script>
+  <!-- Bootstrap 3.3.6 -->
+  <script src="../../bootstrap/js/bootstrap.min.js"></script>
+  <!-- Select2 -->
+  <script src="../../plugins/select2/select2.full.min.js"></script>
+  <!-- DataTables -->
+  <script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
+  <script src="../../plugins/datatables/dataTables.bootstrap.min.js"></script>
+  <!-- date-range-picker -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
+  <script src="../../plugins/daterangepicker/daterangepicker.js"></script>
+  <!-- bootstrap datepicker -->
+  <script src="../../plugins/datepicker/bootstrap-datepicker.js"></script>
+  <!-- SlimScroll 1.3.0 -->
+  <script src="../../plugins/slimScroll/jquery.slimscroll.min.js"></script>
+  <!-- iCheck 1.0.1 -->
+  <script src="../../plugins/iCheck/icheck.min.js"></script>
+  <!-- FastClick -->
+  <script src="../../plugins/fastclick/fastclick.js"></script>
+  <!-- AdminLTE App -->
+  <script src="../../dist/js/app.min.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="../../dist/js/demo.js"></script>
+  <!-- Dark Theme Btn-->
+  <script src="https://dev.colorbiz.org/ashen/cdn/main/dist/js/DarkTheme.js"></script>
 
-return false;
+  <script>
+    function click_open(i) {
+      $("#popup_" + i).removeClass("d-none");
+      $("#container_up").removeClass("d-none");
+    }
 
-});
+    function click_close() {
+      $(".popup").addClass("d-none");
+      $("#container_up").addClass("d-none");
+    }
+  </script>
+  <!-- page script -->
+  <script>
+    $(function() {
+      $(".delbutton").click(function() {
+        //Save the link in a variable called element
+        var element = $(this);
+        //Find the id of the link that was clicked
+        var del_id = element.attr("id");
+        //Built a url to send
+        var info = 'id=' + del_id;
+        if (confirm("Sure you want to delete this Special Price? There is NO undo!")) {
+          $.ajax({
+            type: "GET",
+            url: "special_price_dll.php",
+            data: info,
+            success: function() {
 
-});
-	
-	
-	
-  $(function () {
-    $("#example1").DataTable();
-    $('#example2').DataTable({
-      "paging": true,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": true,
-      "info": true,
-      "autoWidth": false
+            }
+          });
+          $(this).parents(".record").animate({
+              backgroundColor: "#fbc7c7"
+            }, "fast")
+            .animate({
+              opacity: "hide"
+            }, "slow");
+
+        }
+
+        return false;
+
+      });
+
     });
-  });
-	
-	
-	$('#datepicker').datepicker({  autoclose: true, datepicker: true,  format: 'yyyy-mm-dd '});
-    $('#datepicker').datepicker({ autoclose: true });
-	
-	
-	
-	$('#datepickerd').datepicker({  autoclose: true, datepicker: true,  format: 'yyyy-mm-dd '});
-    $('#datepickerd').datepicker({ autoclose: true  });
-	
-</script>
+
+
+
+    $(function() {
+      $("#example1").DataTable();
+      $('#example2').DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false
+      });
+    });
+
+
+    $('#datepicker').datepicker({
+      autoclose: true,
+      datepicker: true,
+      format: 'yyyy-mm-dd '
+    });
+    $('#datepicker').datepicker({
+      autoclose: true
+    });
+
+
+
+    $('#datepickerd').datepicker({
+      autoclose: true,
+      datepicker: true,
+      format: 'yyyy-mm-dd '
+    });
+    $('#datepickerd').datepicker({
+      autoclose: true
+    });
+  </script>
 </body>
+
 </html>
--- customer_category_save.php
+++ customer_category_save.php
@@ -1,24 +1,16 @@
 <?php
 
 session_start();
-
 include('connect.php');
+
+
 $name = $_POST['name'];
-
-//$g = $_POST['root'];
-
-
 
 // query
 
 $sql = "INSERT INTO customer_category ( name ) VALUES (:a)";
 $q = $db->prepare($sql);
-$q->execute(array(':a'=>$name));
+$q->execute(array(':a' => $name));
+
 
 header("location: customer_category.php");
-
-
-
-
-
-?>
