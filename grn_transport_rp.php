d36f8fe80092943038127c79101f909beabd9a4b
ashen <hansakagaa@gmail.com>
2024-03-20 08:50:48 +0530

    transport expenses

--- grn_transport_rp.php
+++ grn_transport_rp.php
@@ -37,9 +37,21 @@ date_default_timezone_set("Asia/Colombo");
     <!-- Main content -->
     <section class="content">
 
+      <?php
+      include("connect.php");
+      date_default_timezone_set("Asia/Colombo");
+
+      if (isset($_GET['lorry'])) {
+
+        $lorry = $_GET['lorry'];
+        $dates = $_GET['dates'];
+        $from = date_format(date_create(explode("-", $dates)[0]), "Y-m-d");
+        $to = date_format(date_create(explode("-", $dates)[1]), "Y-m-d");
+      }
+      ?>
+
       <div class="row">
-        <div class="col-md-2"></div>
-        <div class="col-md-8">
+        <div class="col-md-7">
           <div class="box">
             <div class="box-header with-border">
               <h3 class="box-title">Date Selector</h3>
@@ -91,23 +103,44 @@ date_default_timezone_set("Asia/Colombo");
             </div>
           </div>
         </div>
+        <?php if (isset($_GET['lorry'])) {
+          $total_gross = 0;
+          $total_exp = 0;
+
+          $sql1 = "SELECT * FROM transport_record WHERE lorry_id = '$lorry' AND date BETWEEN '$from' AND '$to'";
+          $sql2 = " SELECT * FROM expenses_records  WHERE  date BETWEEN '$from' AND '$to' ";
+
+          $result = $db->prepare($sql1);
+          $result->bindParam(':userid', $date);
+          $result->execute();
+          for ($i = 0; $row = $result->fetch(); $i++) {
+            $total_gross += $row['amount'];
+          }
+
+          $result = $db->prepare($sql2);
+          $result->bindParam(':userid', $date);
+          $result->execute();
+          for ($i = 0; $row = $result->fetch(); $i++) {
+            $total_exp += $row['amount'];
+          }
+        ?>
+          <div class="col-md-5">
+            <div class="box">
+              <div class="box-header with-border">
+                <h3 class="box-title">Payment Summary</h3>
+              </div>
+              <div class="box-body">
+
+                <h4>Total Gross: <small style="margin-right: 20px;">Rs.</small> <?php echo number_format($total_gross, 2); ?></h4>
+                <h4>Total Expenses: <small style="margin-right: 20px;">Rs.</small> <?php echo number_format($total_exp, 2); ?></h4>
+                <h3>NET Profit: <small style="margin-right: 20px;">Rs.</small> <?php echo number_format($total_gross - $total_exp, 2); ?></h3>
+
+              </div>
+            </div>
+          </div>
+        <?php } ?>
       </div>
 
-
-      <?php
-      include("connect.php");
-      date_default_timezone_set("Asia/Colombo");
-
-      if (isset($_GET['lorry'])) {
-
-        $lorry = $_GET['lorry'];
-        $dates = $_GET['dates'];
-        $from = date_format(date_create(explode("-", $dates)[0]), "Y-m-d");
-        $to = date_format(date_create(explode("-", $dates)[1]), "Y-m-d");
-
-        $sql = "SELECT * FROM transport_record WHERE lorry_id = '$lorry' AND date BETWEEN '$from' AND '$to'";
-      }
-      ?>
       <div class="box box-info">
 
         <div class="box-header with-border">
@@ -127,10 +160,10 @@ date_default_timezone_set("Asia/Colombo");
               </tr>
             </thead>
             <tbody>
-              <?php $tot = 0;
+              <?php $total_gross = 0;
               if (isset($_GET['lorry'])) {
 
-                $result = $db->prepare($sql);
+                $result = $db->prepare($sql1);
                 $result->bindParam(':userid', $date);
                 $result->execute();
                 for ($i = 0; $row = $result->fetch(); $i++) {
@@ -143,7 +176,7 @@ date_default_timezone_set("Asia/Colombo");
                       <td><?php echo $row['lorry_no']  ?></td>
                       <td><?php echo $row['date']  ?></td>
                       <td><?php echo $row['amount'];
-                          $tot += $row['amount'];  ?></td>
+                          $total_gross += $row['amount'];  ?></td>
                     </tr>
               <?php }
                 }
@@ -154,11 +187,77 @@ date_default_timezone_set("Asia/Colombo");
             </tfoot>
           </table>
           <div style="padding-left: 25px;margin-top: 20px;">
-            <h4>Total: <small> Rs. </small> <?php echo number_format($tot, 2); ?> </h4>
+            <h4>Total: <small> Rs. </small> <?php echo number_format($total_gross, 2); ?> </h4>
           </div>
         </div>
 
       </div>
+
+      <div class="box box-info">
+        <div class="box-header with-border">
+          <h3 class="box-title">Expenses Payment</h3>
+        </div>
+
+        <div class="box-body d-block">
+          <table id="example1" class="table table-bordered " style="border-radius: 0;">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Date</th>
+                <th>Type</th>
+                <th>Comment</th>
+                <th>Pay Type</th>
+                <th>Chq Details</th>
+                <th>Amount (Rs.)</th>
+                <th>#</th>
+              </tr>
+            </thead>
+            <tbody>
+
+              <?php $total_exp = 0;
+              $sql = " SELECT * FROM expenses_records  WHERE  date BETWEEN '$from' AND '$to' ";
+              $result = $db->prepare($sql2);
+              $result->bindParam(':userid', $date);
+              $result->execute();
+              for ($i = 0; $row = $result->fetch(); $i++) {
+                $dll = $row['dll'];
+                $type = $row['type_id'];
+                if ($dll == 1) {
+                  $style = 'opacity: 0.5;cursor: default;';
+                } else {
+                  $style = '';
+                }
+              ?>
+
+                <tr class="record" style="<?php echo $style; ?>">
+                  <td><?php echo $row['id'];   ?> </td>
+                  <td><?php echo $row['date'];   ?> </td>
+                  <td>
+                    <?php echo $row['type']; ?>
+                    <br> <span class="badge bg-green"><?php echo $row['lorry_no']; ?> </span>
+                  </td>
+                  <td><?php echo $row['comment'];   ?></td>
+                  <td><?php echo $row['pay_type'];   ?></td>
+                  <td>
+                    NO: <span class="badge bg-blue"><?php echo $row['chq_no']; ?> </span> <br>
+                    Date: <span class="badge bg-green"><?php echo $row['chq_date']; ?> </span> <br>
+                  </td>
+                  <td>Rs.<?php echo $row['amount'];
+                          $total_exp += $row['amount'];  ?>
+                  </td>
+                  <td> <?php if ($dll == 0) { ?> <a href="#" id="<?php echo $row['id']; ?>" class="delbutton btn btn-danger" title="Click to Delete">
+                        <i class="icon-trash">x</i></a><?php } ?>
+                  </td>
+                </tr>
+              <?php }   ?>
+            </tbody>
+          </table>
+          <div style="padding-left: 25px;margin-top: 20px;">
+            <h4>Total: <small> Rs. </small> <?php echo number_format($total_exp, 2); ?> </h4>
+          </div>
+        </div>
+      </div>
+
     </section>
 
   </div>
@@ -199,6 +298,7 @@ date_default_timezone_set("Asia/Colombo");
   <script type="text/javascript">
     $(function() {
       $("#example").DataTable();
+      $("#example1").DataTable();
       $('#example2').DataTable({
         "paging": true,
         "lengthChange": false,
@@ -207,6 +307,40 @@ date_default_timezone_set("Asia/Colombo");
         "info": true,
         "autoWidth": false
       });
+    });
+    $(function() {
+
+
+      $(".delbutton").click(function() {
+
+        //Save the link in a variable called element
+        var element = $(this);
+
+        //Find the id of the link that was clicked
+        var del_id = element.attr("id");
+
+        //Built a url to send
+        var info = 'id=' + del_id;
+        if (confirm("Sure you want to delete this Collection? There is NO undo!")) {
+
+          $.ajax({
+            type: "GET",
+            url: "expenses_dll.php",
+            data: info,
+            success: function() {}
+          });
+          $(this).parents(".record").css({
+            'opacity': '0.5',
+            'cursor': 'default'
+          })
+          $(this).remove();
+
+        }
+
+        return false;
+
+      });
+
     });
   </script>
 
