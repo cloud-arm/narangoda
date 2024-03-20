156465266dd89ca5e902985f4d701840de0be82c
ashen <hansakagaa@gmail.com>
2024-03-20 11:33:20 +0530

    up

--- lorry_dll.php
+++ lorry_dll.php
@@ -9,6 +9,6 @@ $id = $_GET['id'];
 
 
 
-$result = $db->prepare("DELETE FROM lorry WHERE  lorry_id= :memid");
-$result->bindParam(':memid', $id);
+$result = $db->prepare("DELETE FROM lorry WHERE  lorry_id= :id");
+$result->bindParam(':id', $id);
 $result->execute();
