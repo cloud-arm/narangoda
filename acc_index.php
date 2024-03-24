8645d034059c690a049f394084cbaceb02c412c6
ashen <hansakagaa@gmail.com>
2024-03-24 11:13:43 +0530

    up

--- acc_index.php
+++ acc_index.php
@@ -138,70 +138,29 @@ include("connect.php");
 
 
             <div class="row">
-                <div class="col-md-6 hidden">
+                <div class="col-md-6">
                     <!-- LINE CHART -->
-                    <div class="box box-solid bg-teal-gradient">
-                        <div class="box-header">
-                            <i class="fa fa-th"></i>
-
-                            <h3 class="box-title">Sales Graph</h3>
+                    <div class="box box-info">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Line Chart</h3>
 
                             <div class="box-tools pull-right">
-                                <button type="button" class="btn bg-teal btn-sm" data-widget="collapse"><i class="fa fa-minus"></i>
+                                <button type="button" class="btn btn-box-tool" data-widget="collapse">
+                                    <i class="fa fa-minus"></i>
                                 </button>
-                                <button type="button" class="btn bg-teal btn-sm" data-widget="remove"><i class="fa fa-times"></i>
+                                <button type="button" class="btn btn-box-tool" data-widget="remove">
+                                    <i class="fa fa-times"></i>
                                 </button>
                             </div>
                         </div>
-                        <div class="box-body border-radius-none">
-                            <div class="chart" id="line-chart" style="height: 300px;"></div>
-                        </div>
-                    </div>
-                </div>
-                <!-- /.col (LEFT) -->
-                <div class="col-md-6 hidden">
-                    <!-- BAR CHART -->
-                    <div class="box box-solid ">
-                        <div class="box-header ">
-                            <h3 class="box-title">IN AND OUT Chart</h3>
-                            <div class="box-tools pull-right">
-                                <button type="button" class="btn  btn-sm" data-widget="collapse"><i class="fa fa-minus"></i>
-                                </button>
-                                <button type="button" class="btn btn-sm" data-widget="remove"><i class="fa fa-times"></i>
-                                </button>
+                        <div class="box-body">
+                            <div class="chart">
+                                <canvas id="lineChart1" style="height: 250px"></canvas>
                             </div>
-                        </div>
-                        <div class="box-body chart-responsive">
-                            <div class="chart" id="bar-chart" style="height: 300px;"></div>
                         </div>
                         <!-- /.box-body -->
                     </div>
-                    <!-- /.box -->
-
                 </div>
-                <!-- /.col (RIGHT) -->
-
-                <div class="col-md-12">
-                    <!-- BAR CHART -->
-                    <div class="box box-solid ">
-                        <div class="box-header ">
-                            <h3 class="box-title">14 Day Payment Chart</h3>
-                            <div class="box-tools pull-right">
-                                <button type="button" class="btn  btn-sm" data-widget="collapse"><i class="fa fa-minus"></i>
-                                </button>
-                                <button type="button" class="btn btn-sm" data-widget="remove"><i class="fa fa-times"></i>
-                                </button>
-                            </div>
-                        </div>
-                        <div class="box-body chart-responsive">
-                            <div class="chart" id="bar-chart2"></div>
-                        </div>
-                        <!-- /.box-body -->
-                    </div>
-                    <!-- /.box -->
-
-                </div>
-
             </div>
 
         </section>
@@ -227,6 +186,8 @@ include("connect.php");
     <!-- Morris.js charts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
     <script src="../../plugins/morris/morris.min.js"></script>
+    <!-- ChartJS -->
+    <script src="../../plugins/chartjs/Chart.min.js"></script>
     <!-- FastClick -->
     <script src="../../plugins/fastclick/fastclick.js"></script>
     <!-- AdminLTE App -->
@@ -238,168 +199,85 @@ include("connect.php");
 
     <script>
         $(function() {
-            "use strict";
-
-
-
-            // LINE CHART
-            var line = new Morris.Line({
-                element: 'line-chart',
-                resize: true,
-                data: [{
-                        y: '2019 Q1',
-                        item1: 26006060
-                    },
-                    {
-                        y: '2019 Q2',
-                        item1: 27007080
-                    },
-                    {
-                        y: '2019 Q3',
-                        item1: 49001020
-                    },
-                    {
-                        y: '2019 Q4',
-                        item1: 37006070
-                    },
-                    {
-                        y: '2020 Q1',
-                        item1: 68001000
-                    },
-                    {
-                        y: '2020 Q2',
-                        item1: 56007000
-                    },
-                    {
-                        y: '2020 Q3',
-                        item1: 48002000
-                    },
-                    {
-                        y: '2020 Q4',
-                        item1: 150007030
-                    },
-                    {
-                        y: '2021 Q1',
-                        item1: 106008070
-                    },
-                    {
-                        y: '2021 Q2',
-                        item1: 84003020
-                    }
+            var lineChartData = {
+                labels: [
+                    "January",
+                    "February",
+                    "March",
+                    "April",
+                    "May",
+                    "June",
+                    "July",
                 ],
-                xkey: 'y',
-                ykeys: ['item1'],
-                labels: ['Value'],
-                lineColors: ['#ffffff'],
-                gridTextColor: ['#ffffff'],
-                hideHover: 'auto'
-            });
-
-
-
-            //BAR CHART
-            var bar = new Morris.Bar({
-                element: 'bar-chart',
-                resize: true,
-                data: [{
-                        y: '2015',
-                        a: 10000000,
-                        b: 9000000
+                datasets: [{
+                        label: "Electronics",
+                        fillColor: "rgba(255, 0, 0, 1)",
+                        strokeColor: "rgba(255, 0, 0, 1)",
+                        pointColor: "rgba(255, 0, 0, 1)",
+                        pointStrokeColor: "#c1c7d1",
+                        pointHighlightFill: "#fff",
+                        pointHighlightStroke: "rgba(220,220,220,1)",
+                        data: [65, 59, 80, 81, 56, 55, 100],
                     },
                     {
-                        y: '2016',
-                        a: 7500000,
-                        b: 6500000
+                        label: "Digital Goods",
+                        fillColor: "rgba(0,0,255,1)",
+                        strokeColor: "rgba(0,0,255,1)",
+                        pointColor: "#3b8bba",
+                        pointStrokeColor: "rgba(0,0,255,1)",
+                        pointHighlightFill: "#fff",
+                        pointHighlightStroke: "rgba(0,0,255,1)",
+                        data: [28, 48, 40, 19, 86, 27, 90],
                     },
-                    {
-                        y: '2017',
-                        a: 5000000,
-                        b: 4000000
-                    },
-                    {
-                        y: '2018',
-                        a: 7500000,
-                        b: 6500000
-                    },
-                    {
-                        y: '2019',
-                        a: 5000000,
-                        b: 4000000
-                    },
-                    {
-                        y: '2020',
-                        a: 7500000,
-                        b: 6500000
-                    },
-                    {
-                        y: '2021',
-                        a: 7500000,
-                        b: 9700000
-                    }
                 ],
-                barColors: ['#00a65a', '#f56954'],
-                xkey: 'y',
-                ykeys: ['a', 'b'],
-                labels: ['IN', 'OUT'],
-                hideHover: 'auto'
-            });
+            };
 
+            var lineChartOptions = {
+                //Boolean - If we should show the scale at all
+                showScale: true,
+                //Boolean - Whether grid lines are shown across the chart
+                scaleShowGridLines: false,
+                //String - Colour of the grid lines
+                scaleGridLineColor: "rgba(0,0,0,.05)",
+                //Number - Width of the grid lines
+                scaleGridLineWidth: 1,
+                //Boolean - Whether to show horizontal lines (except X axis)
+                scaleShowHorizontalLines: true,
+                //Boolean - Whether to show vertical lines (except Y axis)
+                scaleShowVerticalLines: true,
+                //Boolean - Whether the line is curved between points
+                bezierCurve: true,
+                //Number - Tension of the bezier curve between points
+                bezierCurveTension: 0.3,
+                //Boolean - Whether to show a dot for each point
+                pointDot: false,
+                //Number - Radius of each point dot in pixels
+                pointDotRadius: 4,
+                //Number - Pixel width of point dot stroke
+                pointDotStrokeWidth: 1,
+                //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+                pointHitDetectionRadius: 20,
+                //Boolean - Whether to show a stroke for datasets
+                datasetStroke: true,
+                //Number - Pixel width of dataset stroke
+                datasetStrokeWidth: 2,
+                //Boolean - Whether to fill the dataset with a color
+                datasetFill: true,
+                //String - A legend template
+                legendTemplate: '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].lineColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
+                //Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+                maintainAspectRatio: true,
+                //Boolean - whether to make the chart responsive to window resizing
+                responsive: true,
+            };
 
-            //BAR CHART
-            var bar = new Morris.Bar({
-                element: 'bar-chart2',
-                resize: true,
-                data: [
-                    <?php $x = 0;
-                    while ($x <= 14) {
-                        $d = strtotime("-$x Day");
-                        $date = date("Y-m-d", $d);
-                        $cash = 0;
-                        $chq = 0;
-                        $credit = 0;
-                        $result1 = $db->prepare("SELECT  amount FROM payment WHERE date='$date' AND action > '0' AND type='cash' ");
-                        $result1->bindParam(':userid', $date);
-                        $result1->execute();
-                        for ($i = 0; $row1 = $result1->fetch(); $i++) {
-                            $cash += $row1['amount'];
-                        }
-                        $result1 = $db->prepare("SELECT  amount FROM payment WHERE date='$date' AND action > '0' AND type='chq' ");
-                        $result1->bindParam(':userid', $date);
-                        $result1->execute();
-                        for ($i = 0; $row1 = $result1->fetch(); $i++) {
-                            $chq += $row1['amount'];
-                        }
-
-                        $result1 = $db->prepare("SELECT  amount FROM payment WHERE date='$date' AND action > '0' AND type='credit' ");
-                        $result1->bindParam(':userid', $date);
-                        $result1->execute();
-                        for ($i = 0; $row1 = $result1->fetch(); $i++) {
-                            $credit += $row1['amount'];
-                        }
-
-                        $split = explode("-", $date);
-                        $y = $split[0];
-                        $m = $split[1];
-                        $d = $split[2];
-                        $date = mktime(0, 0, 0, $m, $d, $y);
-                        $date = date('M d', $date);
-
-                    ?> {
-                            x: '<?php echo $date; ?>',
-                            a: <?php echo $cash; ?>,
-                            b: <?php echo $chq; ?>,
-                            c: <?php echo $credit; ?>
-                        },
-                    <?php $x++;
-                    } ?>
-
-                ],
-                barColors: ['#ff9900', '#8c8c8c', '#cc0000'],
-                xkey: 'x',
-                ykeys: ['a', 'b', 'c'],
-                labels: ['CASH', 'CHQ', 'CREDIT'],
-                hideHover: 'auto'
-            });
+            //-------------
+            //- LINE CHART -
+            //--------------
+            var lineChartCanvas = $("#lineChart1").get(0).getContext("2d");
+            var lineChart = new Chart(lineChartCanvas);
+            lineChartOptions.datasetFill = false;
+            lineChart.Line(lineChartData, lineChartOptions);
         });
     </script>
 </body>
